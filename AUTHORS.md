5bfddca4c2cc5d578fc913bad441918cc66ca44b
pacargile <pcargile@cfa.harvard.edu>
2017-09-28 13:43:28 -0400

    initial commit

---
+++ AUTHORS.md
@@ -0,0 +1,7 @@
+Author:
+
+- [Phillip Cargile](https://github.com/pacargile)
+
+Contributors:
+
+- [Ben Johnson](https://github.com/bd-j)
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import re
+import glob
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+    
+setup(
+    name="MINESweeper",
+    url="https://github.com/pacargile/MINESweeper_V2.0",
+    version="2.0",
+    author="Phillip Cargile",
+    author_email="pcargile@cfa.harvard.edu",
+    packages=["MINESweeper"],
+    license="LICENSE",
+    description="MIST Isochrones w/ Nested Sampling",
+    long_description=open("README.md").read(),
+    package_data={"": ["README.md", "LICENSE"]},
+    include_package_data=True,
+    install_requires=["numpy", "scipy", "dynesty"],
+)
+
