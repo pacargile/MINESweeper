e88723243c84c834609b32ce241e19ad79408c67
pacargile <pcargile@cfa.harvard.edu>
2025-07-16 16:29:40 -0400

    updating code

--- minesweeper/fitstar.py
+++ minesweeper/fitstar.py
@@ -367,8 +367,8 @@ class FitMS(object):
                maxiter=maxiter,
                maxcall=maxcall,
                )):
-               (worst, ustar, vstar, loglstar, logvol, logwt, logz, logzvar,
-                    h, nc, worst_it, propidx, propiter, eff, delta_logz) = results             
+               print('Length of results: {0}'.format(len(results)))
+               (worst, ustar, vstar, loglstar, logvol, logwt, logz, logzvar, h, nc, worst_it, propidx, propiter, eff, delta_logz) = results             
 
                if it == 0:
                     # initialize the output file
